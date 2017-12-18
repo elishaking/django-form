82d2ac398bc3342197b05dffc436f0012f0a81a4
chiapeter <chiapeterakaata@gmail.com>
2017-12-18 17:29:41 +0100

    added fields to admin

--- regform/admin.py
+++ regform/admin.py
@@ -22,7 +22,17 @@ class MembersAdmin(admin.ModelAdmin):
         'catechetical_work', 'liturgical_work', 'security_work', 'environmental_work', 'health_work', 'sports',
         'other_work')
 
-    search_fields = ('full_name', 'state', 'lga', 'phone')
+    search_fields = ('member_id', 'full_name', 'phone', 'marital_status', 'residence', 'state', 'lga', 'email', 'gender',
+        'name_of_next_of_kin',
+        'address_of_next_of_kin', 'relation_with_next_of_kin',
+        'occupation', 'business_address', 'skills', 'sports',
+        'are_you_a_baptized_catholic', 'not_baptized', 'are_you_a_communicant', 'not_communicant', 'are_you_confirmed',
+        'not_confirmed', 'are_wedded_in_the_church', 'not_wedded', 'any_tribal_community', 'not_in_tribal_community',
+        'in_tribal_community',
+        'member_of_any_pius_society', 'not_in_pius_society', 'yes_In_pius_society', 'belongs_to_any_organ_in_church',
+        'dont_belong_to', 'yes_belong_to',
+        'catechetical_work', 'liturgical_work', 'security_work', 'environmental_work', 'health_work', 'sports',
+        'other_work')
 
     # fields = ['phone', 'marital_status', 'residence', 'state', 'lga', 'town', 'email', 'gender',
     # 'name_of_next_of_kin', 'address_of_next_of_kin', 'relation_with_next_of_kin', 'occupation', 'business_address',
